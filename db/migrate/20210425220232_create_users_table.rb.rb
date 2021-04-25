2f9041e6fb5f4b2ab89a20af820e032b70d05772
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-04-25 18:07:29 -0400

    add user model file and migration table

---
+++ db/migrate/20210425220232_create_users_table.rb.rb
@@ -0,0 +1,9 @@
+class CreateUsersTable.rb < ActiveRecord::Migration[6.1]
+  def change
+    create_table : users do |t|
+    t.string :name
+    t.string :email
+    t.string :password_digest
+    end
+  end
+end
